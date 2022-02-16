cb9d49b2b436fd72682bc258056fac460fed6368
Stefan Huettenmoser <stefan.huettenmoser@gmail.com>
2022-02-16 20:18:03 +0100

    Fix Wording

--- component_layouts/input.py
+++ component_layouts/input.py
@@ -4,17 +4,17 @@ from components import PihomeComponent
 
 
 class Input(PihomeComponent):
-    def __init__(self, db, name, input_pin, value_type):
+    def __init__(self, db, name, output_pin, value_type):
         super().__init__(db, name)
 
-        self.input_pin = input_pin
+        self.output_pin = output_pin
         self.value_type = value_type
         self.db.init_table(self.name, self.value_type)
 
         self.pi = pigpio.pi()
-        self.pi.set_mode(self.input_pin, pigpio.INPUT)
+        self.pi.set_mode(self.output_pin, pigpio.INPUT)
 
     def run(self):
-        value = self.pi.read(self.input_pin)
+        value = self.pi.read(self.output_pin)
         self.db.add_one(self.name, value)
-        return f"Read form Pin-{self.input_pin}: {value} and Save to Database '{self.name}'"
+        return f"Read form Pin-{self.output_pin}: {value} and Save to Database '{self.name}'"
