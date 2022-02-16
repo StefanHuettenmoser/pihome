235c932042d1e977fa598b3339549e37d8afcdba
Stefan Huettenmoser <stefan.huettenmoser@gmail.com>
2022-02-16 19:38:31 +0100

    Add timeout function to switch

--- component_layouts/switch.py
+++ component_layouts/switch.py
@@ -1,14 +1,16 @@
 import subprocess
+from time import sleep
 
 from components import PihomeComponent
 
 
 class Switch(PihomeComponent):
-    def __init__(self, db, name, input_pin, state=0):
+    def __init__(self, db, name, input_pin, state=0, timeout=0):
         super().__init__(db, name)
 
         self.input_pin = input_pin
         self.state = state
+        self.timeout = timeout
 
         self._execute(f"pigs modes {self.input_pin} w")
 
@@ -18,4 +20,8 @@ class Switch(PihomeComponent):
 
     def run(self):
         self._execute(f"pigs w {self.input_pin} {1 if self.state else 0}")
-        return f"Switch Pin-{self.input_pin} {'on' if self.state else 'off'}"
+        if self.timeout:
+            sleep(self.timeout)
+            self._execute(f"pigs w {self.input_pin} {0 if self.state else 1}")
+
+        return f"Switch Pin-{self.input_pin} {'on' if self.state else 'off'}{f' for {self.timeout} seconds' if self.timeout else ''}"
