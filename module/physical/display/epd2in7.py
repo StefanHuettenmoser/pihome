b288547efe64f19c1fe3f33c84db5e6638aaf0f8
Stefan Huettenmoser <stefan.huettenmoser@gmail.com>
2022-02-24 00:27:09 +0100

    Add ability to display data from data warehouse

--- module/physical/display/epd2in7.py
+++ module/physical/display/epd2in7.py
@@ -1,6 +1,8 @@
 # https://github.com/waveshare/e-Paper/blob/master/RaspberryPi_JetsonNano/python/examples/epd_2in7_test.py
 import logging
 
+from database import Logic
+
 logging.basicConfig(level=logging.DEBUG)
 
 import threading
@@ -14,37 +16,18 @@ from .layouts import BasicList
 
 from modules import PihomeActor
 
-# class PaperScreen:
-#     def __init__(self):
-#         logging.info("set up")
-#         GPIO.setmode(GPIO.BCM)
-
-#         self.epd = epd2in7.EPD()
-
-#         logging.info("init and clear")
-#         self.epd.init()
-#         self.epd.Clear(0xFF)
-#         self.epd.Init_4Gray()
-
-#         # self.h = self.epd.height
-#         # self.w = self.epd.width
-
-#     def draw_screen(self, image):
-#         self.epd.display_4Gray(self.epd.getbuffer_4Gray(image))
-#         self.epd.sleep()
-
-#     def get_colors(self):
-#         return (self.epd.GRAY1, self.epd.GRAY2, self.epd.GRAY3, self.epd.GRAY4)
-
 
 class List(PihomeActor):
-    def __init__(self, pi, db, name, stage, every):
+    def __init__(self, pi, db, name, stage, every, title, line, values):
         super().__init__(pi, db, name, stage, every)
-        # GPIO.setmode(GPIO.BCM)
+
+        self.title = title
+        self.line = line
+        self.value_logics = (Logic(value) for value in values)
+
         self.epd = epd2in7.EPD()
         self.epd.init()
         self.epd.Clear(0xFF)
-        self.epd.Init_4Gray()
 
         ubuntu_mono_regular = "Ubuntu_Mono/UbuntuMono-Regular.ttf"
         ubuntu_mono_bold = "Ubuntu_Mono/UbuntuMono-Bold.ttf"
@@ -54,19 +37,25 @@ class List(PihomeActor):
         pixel_font_18 = ImageFont.truetype(os.path.join(rsc_dir, ubuntu_mono_bold), 18)
 
         self.basic_list = BasicList(
-            title="TestList",
+            title=self.title,
             width=self.epd.width,
             height=self.epd.height,
             colors=self.__get_colors(),
             font_text=pixel_font_14,
             font_title=pixel_font_18,
         )
-        self.basic_list.add_element("Your data could be displayed here!")
 
     def __get_colors(self):
         return (self.epd.GRAY1, self.epd.GRAY2, self.epd.GRAY3, self.epd.GRAY4)
 
     def draw_screen(self, callback):
+        self.basic_list.add_element(
+            self.line.format(
+                *(value_logic.get_value(self.db) for value_logic in self.value_logics)
+            )
+        )
+
+        self.epd.Init_4Gray()
         self.epd.display_4Gray(self.epd.getbuffer_4Gray(self.basic_list.get_image()))
         self.epd.sleep()
         callback()
