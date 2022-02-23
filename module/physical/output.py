2e16c1adc6644ae9a7a9fda03a78b9b569eff18d
Stefan Huettenmoser <stefan.huettenmoser@gmail.com>
2022-02-23 15:41:32 +0100

    Add Debugging Output

--- module/physical/output.py
+++ module/physical/output.py
@@ -6,6 +6,17 @@ from modules import PihomeActor
 from database import Logic
 
 
+class Debug(PihomeActor):
+    def __init__(self, pi, db, name, stage, state):
+        super().__init__(pi, db, name, stage)
+        self.state_logic = Logic(state)
+
+    def perform(self, callback):
+        state = self.state_logic.get_value(self.db)
+        callback()
+        return f"Debugging Output {self.name}: {state}"
+
+
 class Output(PihomeActor):
     def __init__(self, pi, db, name, stage, input_pin, state=0, timeout=0):
         super().__init__(pi, db, name, stage)
@@ -14,19 +25,19 @@ class Output(PihomeActor):
         self.state_logic = Logic(state)
         self.timeout = timeout
 
-        self._execute(f"pigs modes {self.input_pin} w")
+        self.___execute(f"pigs modes {self.input_pin} w")
 
-    def _execute(self, command):
+    def ___execute(self, command):
         process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
         return process.communicate()
 
     def perform(self, callback):
         state = self.state_logic.get_value(self.db)
-        self._execute(f"pigs w {self.input_pin} {1 if state else 0}")
+        self.___execute(f"pigs w {self.input_pin} {1 if state else 0}")
         if self.timeout:
 
             def revert():
-                self._execute(f"pigs w {self.input_pin} {0 if state else 1}")
+                self.___execute(f"pigs w {self.input_pin} {0 if state else 1}")
                 callback()
 
             timer = threading.Timer(self.timeout, revert)
