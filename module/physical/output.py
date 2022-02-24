8b106410a1b6893d6e6770cd1a5b83fb39b95f19
Stefan Huettenmoser <stefan.huettenmoser@gmail.com>
2022-02-24 10:57:18 +0100

    Add timeout feature to PWM Output

--- module/physical/output.py
+++ module/physical/output.py
@@ -17,13 +17,31 @@ class Debug(PihomeActor):
         return f"Debugging Output {self.name}: {state}"
 
 
-class Output(PihomeActor):
-    def __init__(self, pi, db, name, stage, every, input_pin, state=0, timeout=0):
+class TimeoutOutput(PihomeActor):
+    def __init__(self, pi, db, name, stage, every, timeout):
         super().__init__(pi, db, name, stage, every)
+        self.timeout = timeout
+
+    def timeout_action(self, action, revert_action, callback):
+        action()
+        if self.timeout:
+
+            def revert():
+                revert_action()
+                callback()
+
+            timer = threading.Timer(self.timeout, revert)
+            timer.start()
+        else:
+            callback()
+
+
+class Output(TimeoutOutput):
+    def __init__(self, pi, db, name, stage, every, input_pin, timeout=0, state=0):
+        super().__init__(pi, db, name, stage, every, timeout)
 
         self.input_pin = input_pin
         self.state_logic = Logic(state)
-        self.timeout = timeout
 
         self.___execute(f"pigs modes {self.input_pin} w")
 
@@ -33,21 +51,19 @@ class Output(PihomeActor):
 
     def perform(self, callback):
         state = self.state_logic.get_value(self.db)
-        self.___execute(f"pigs w {self.input_pin} {1 if state else 0}")
-        if self.timeout:
 
-            def revert():
-                self.___execute(f"pigs w {self.input_pin} {0 if state else 1}")
-                callback()
+        def action():
+            self.___execute(f"pigs w {self.input_pin} {1 if state else 0}")
 
-            timer = threading.Timer(self.timeout, revert)
-            timer.start()
-        else:
-            callback()
+        def revert_action():
+            self.___execute(f"pigs w {self.input_pin} {0 if state else 1}")
+
+        self.timeout_action(action, revert_action, callback)
+
         return f"Switch Pin-{self.input_pin} {'on' if state else 'off'}{f' for {self.timeout} seconds' if self.timeout else ''}"
 
 
-class PWMOutput(PihomeActor):
+class PWMOutput(TimeoutOutput):
     def __init__(
         self,
         pi,
@@ -57,22 +73,32 @@ class PWMOutput(PihomeActor):
         every,
         input_pin,
         frequency,
+        timeout=0,
         duty_cycle=0.5,
         hardware_PWM=False,
     ):
-        super().__init__(pi, db, name, stage, every)
+        super().__init__(pi, db, name, stage, every, timeout)
         self.input_pin = input_pin
         self.frequency_logic = Logic(frequency)
         self.duty_cycle_logic = Logic(duty_cycle)
         self.hardware_PWM = hardware_PWM
 
-    def perform(self, callback):
-        frequency = self.frequency_logic.get_value()
-        duty_cycle = self.duty_cycle_logic.get_value()
+    def set_pwm(self, frequency, duty_cycle):
         if self.hardware_PWM:
             self.pi.hardware_PWM(self.input_pin, frequency, int(1_000_000 * duty_cycle))
         else:
             self.pi.set_PWM_frequency(self.input_pin, frequency)
             self.pi.set_PWM_dutycycle(self.input_pin, int(duty_cycle * 255))
 
-        callback()
+    def perform(self, callback):
+        frequency = self.frequency_logic.get_value()
+        duty_cycle = self.duty_cycle_logic.get_value()
+
+        def action():
+            self.set_pwm(frequency, duty_cycle)
+
+        def revert_action():
+            self.set_pwm(0, 0)
+
+        self.timeout_action(action, revert_action, callback)
+        return f"Switch Pin-{self.input_pin} to {frequency:.2f}Hz @{int(duty_cycle)*100}% {f' for {self.timeout} seconds' if self.timeout else ''}"
