13c29eadedc5de056ef5c2823e8b7c1b0c33c0d7
Stefan Huettenmoser <stefan.huettenmoser@gmail.com>
2022-02-24 10:00:55 +0100

    Add multiline feature

--- module/physical/display/epd2in7.py
+++ module/physical/display/epd2in7.py
@@ -18,12 +18,26 @@ from modules import PihomeActor
 
 
 class List(PihomeActor):
-    def __init__(self, pi, db, name, stage, every, title, line, values):
+    def __init__(
+        self,
+        pi,
+        db,
+        name,
+        stage,
+        every,
+        title: str,
+        lines: list,
+        values: list,
+        body_font_size=14,
+        title_font_size=18,
+    ):
         super().__init__(pi, db, name, stage, every)
 
         self.title = title
-        self.line = line
-        self.value_logics = [Logic(value) for value in values]
+        self.lines = lines
+        self.value_logics = [
+            [Logic(value) for value in line_values] for line_values in values
+        ]
 
         self.epd = epd2in7.EPD()
         self.epd.init()
@@ -31,29 +45,35 @@ class List(PihomeActor):
 
         ubuntu_mono_regular = "Ubuntu_Mono/UbuntuMono-Regular.ttf"
         ubuntu_mono_bold = "Ubuntu_Mono/UbuntuMono-Bold.ttf"
-        pixel_font_14 = ImageFont.truetype(
-            os.path.join(rsc_dir, ubuntu_mono_regular), 14
+        pixel_font_body = ImageFont.truetype(
+            os.path.join(rsc_dir, ubuntu_mono_regular), body_font_size
         )
-        pixel_font_18 = ImageFont.truetype(os.path.join(rsc_dir, ubuntu_mono_bold), 18)
+        pixel_font_title = ImageFont.truetype(
+            os.path.join(rsc_dir, ubuntu_mono_bold), title_font_size
+        )
 
         self.basic_list = BasicList(
             title=self.title,
             width=self.epd.width,
             height=self.epd.height,
             colors=self.__get_colors(),
-            font_text=pixel_font_14,
-            font_title=pixel_font_18,
+            font_text=pixel_font_body,
+            font_title=pixel_font_title,
         )
 
     def __get_colors(self):
         return (self.epd.GRAY1, self.epd.GRAY2, self.epd.GRAY3, self.epd.GRAY4)
 
     def draw_screen(self, callback):
-        self.basic_list.add_element(
-            self.line.format(
-                *(value_logic.get_value(self.db) for value_logic in self.value_logics)
+        for line, line_value_logics in zip(self.lines, self.value_logics):
+            self.basic_list.add_element(
+                line.format(
+                    *(
+                        value_logic.get_value(self.db)
+                        for value_logic in line_value_logics
+                    )
+                )
             )
-        )
 
         self.epd.Init_4Gray()
         self.epd.display_4Gray(self.epd.getbuffer_4Gray(self.basic_list.get_image()))
