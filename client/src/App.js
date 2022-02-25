3ae658fb8eadfd66782dfcd444e0ed0ff5c4c748
Stefan Huettenmoser <stefan.huettenmoser@gmail.com>
2022-02-25 19:38:26 +0100

    Add Mui

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
 import Home from "./containers/Home";
 
 function App() {
 	return (
 		<div className="App">
+			<CssBaseline key="css-baseline" />
 			<Home key="home" />
 		</div>
 	);
