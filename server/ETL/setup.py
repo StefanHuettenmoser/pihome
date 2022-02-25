9ac28648d8aab7a862b545b4582b3f674cf13c01
Stefan Huettenmoser <stefan.huettenmoser@gmail.com>
2022-02-25 17:04:14 +0100

    BUGFIX: should only describe python script

--- server/ETL/setup.py
+++ server/ETL/setup.py
@@ -9,18 +9,19 @@ requirements = [
     for x in (here / "requirements.txt").read_text(encoding="utf-8").split("\n")
     if len(x) > 0
 ]
-long_description = (here / "README.md").read_text(encoding="utf-8")
+# long_description = (here / "README.md").read_text(encoding="utf-8")
+long_description = "Not available yet..."
 
 setup(
     name="pihome",
-    description="A small server thats get data from the RaspberryPi and displays it",
+    description="A modular python script that collects data and stores it in a database",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=["log"],
     version="0.1",
     url="https://github.com/StefanHuettenmoser/pihome",
     license="MIT",
-    author="Stefan Huettenmoser",
+    author="Stefan Huttenmoser",
     author_email="stefan.huettenmoser@gmail.com",
     classifiers=["Development Status :: 3 - Alpha"],
     python_requires=">=3.8",
