588bf4ed96bd5d1e71a7ba80cae9fd221c201a37
Stefan Huettenmoser <stefan.huettenmoser@gmail.com>
2022-03-07 15:49:49 +0100

    BUGFIX: Initialize position at 1

--- client/src/hooks/useWidgets.js
+++ client/src/hooks/useWidgets.js
@@ -72,7 +72,7 @@ export default function useWidgets(columns) {
 	);
 
 	const addWidget = useCallback(async () => {
-		const position = Math.max(...widgetsConfig.map((e) => e.position)) + 1;
+		const position = Math.max(...widgetsConfig.map((e) => e.position)) || 0 + 1;
 		const width = 1;
 		const height = 1;
 		const widget_id = 1;
