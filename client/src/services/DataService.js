4e0ae3d2464ee45ef7ddab31b5a17945622fdb54
Stefan Huettenmoser <stefan.huettenmoser@gmail.com>
2022-03-07 15:15:02 +0100

    Code cleanup

--- client/src/services/DataService.js
+++ client/src/services/DataService.js
@@ -1,15 +1,11 @@
-import {
-	authGET,
-	authPOST,
-	SERVER_API,
-	handleFetch,
-} from "./helpers/authFetch";
+import { authGET, handleFetch } from "./helpers/authFetch";
 
 const DataService = {
 	getTableNames: () => {
 		return handleFetch("getTableNames", authGET("data"));
 	},
 	getTableData: (table_name, start_date, end_date) => {
+		// TODO: Implement start_data and end_date
 		return handleFetch(
 			`getTableData (${table_name})`,
 			authGET(`data/${table_name}`)
